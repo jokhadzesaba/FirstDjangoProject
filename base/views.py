3e890e6b7efb6202ffe633b7c01bb68609ff844d
jokhadzesaba <saba.joxadze.04@gmail.com>
2024-12-08 16:50:52 +0400

    adding edit and delete operations

--- base/views.py
+++ base/views.py
@@ -19,10 +19,25 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save() 
-            rooms = Room.objects.all()
-            print(Room.objects.all()) 
+            form.save()
             return redirect('home')
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
 
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
